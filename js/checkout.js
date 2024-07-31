73cf8a3fd968d6b62acdd5e5f0a1e2ab07ee5c06
Adriana Brandan <145227272+AdriBrandy@users.noreply.github.com>
2024-07-31 02:32:22 -0300

    soporte y us

---
+++ js/checkout.js
@@ -0,0 +1,23 @@
+
+ // JavaScript para la validación del formulario
+ document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', function(event) {
+      event.preventDefault(); 
+      
+      const name = document.getElementById('name').value;
+      const surname = document.getElementById('surname').value;
+      const email = document.getElementById('email').value;
+      const country = document.getElementById('country').value;
+      
+      const message = `
+          Nombre: ${name}\n
+          Apellido: ${surname}\n
+          Email: ${email}\n
+          País de Residencia: ${country}
+      `;
+      
+      alert(message);
+  });
+});
